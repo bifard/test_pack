e43fd9f88a6f3ac70a4664fbb4951f3e2a32e3c5
Bifard-Ios <Bifard-Ios@mail.ru>
2023-08-22 14:06:22 +0700

    test

--- dist/_types/constants.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const HOST_NAME = "https://api.garzdrav.ru:7090";
-export declare const BASE_IMG_URL: string;
-export declare const CONDITIONS_PATH = "/files/images/open/conditions/";
-export declare const PRODUCT_PATH = "/files/images/open/products/";
---
+++ index.d.ts
@@ -0,0 +1,16 @@
+declare module "src/constants" {
+    export const HOST_NAME = "https://api.garzdrav.ru:7090";
+    export const BASE_IMG_URL: string;
+    export const CONDITIONS_PATH = "/files/images/open/conditions/";
+    export const PRODUCT_PATH = "/files/images/open/products/";
+}
+declare module "src/type" {
+    export interface ImageServiceType {
+        getConditionOtherImgUrl: (name: string) => string;
+        getProductImgUrl: (name: string) => string;
+    }
+}
+declare module "src/image-service" {
+    import { ImageServiceType } from "src/type";
+    export const imageService: ImageServiceType;
+}
